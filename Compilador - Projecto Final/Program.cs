87397ff33e64bf86b501b929db99a8e88b30b870
luis cruz <luisangelc811@hotmail.com>
2022-04-08 20:58:20 -0400

    Comentario hola

--- Compilador - Projecto Final/Program.cs
+++ Compilador - Projecto Final/Program.cs
@@ -17,6 +17,7 @@ namespace Compilador___Projecto_Final
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //heloo
         }
     }
 }
